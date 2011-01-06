e2857d42a0829142559fa2cafee3475c2ff84092
Maximiliano Luque <mluque@monits.com>
2011-01-06 18:09:16 +0000

    Initial import

---
+++ src/main/java/com/monits/commons/RandomUtils.java
@@ -0,0 +1,48 @@
+/**
+ * Random Utils.
+ *
+ * @copyright 2011 Monits
+ * @license   Copyright (C) 2011. All rights reserved
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+package com.monits.commons;
+
+import java.util.Random;
+
+/**
+ * Random Utils.
+ *
+ * @author    Maximiliano Luque <mluque@monits.com>
+ * @copyright 2011 Monits
+ * @license   Copyright (C) 2011. All rights reserved
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+public class RandomUtils {
+	
+	/**
+	 * Generates a random String with 60 chars long.
+	 * 
+	 * @return    The random String.
+	 */
+	public static String generateCode(){
+		
+		Random random = new Random();
+		
+		char[] characters = {'0','1','2','3','4','5','6','7','8','9' ,'a',
+				'b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t',
+				'u','v','w','x','y','z'};
+		
+		StringBuffer buffer = new StringBuffer();
+		
+		for (int i = 0; i < 60 ; i++) {
+			buffer.append(characters[random.nextInt(characters.length-1)]);
+		}
+		
+		return buffer.toString();
+	}
+	
+}
