39436189ad2e9aade99d28033b6aa6e40a5983ab
Gaston Muñiz <gmuniz@monits.com>
2011-01-19 18:18:41 +0000

    Added unit tests for RandomUtils, improved documentation to clarify behaviour on edge cases.

--- src/main/java/com/monits/commons/RandomUtils.java
+++ src/main/java/com/monits/commons/RandomUtils.java
@@ -22,30 +22,30 @@ import java.util.Random;
  * @since     1.0.0
  */
 public class RandomUtils {
-	
+
 	/**
-	 * Generates a random String with the number of characters supplied.
-	 * 
-	 * @param amount The number of characters in the string.
-	 * 
+	 * Generates a random String with the number of characters requested.
+	 *
+	 * @param amount The number of characters in the string. If negative, it will be treated as zero.
+	 *
 	 * @return The random String.
 	 */
-	public static String generateRandomString(int amount){
-		
+	public static String generateRandomString(int amount) {
+
 		Random random = new Random();
-		
+
 		char[] characters = {'0','1','2','3','4','5','6','7','8','9' ,'a','b',
 				'c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p',
 				'q','r','s','t','u','v','w','x','y','z'
 				};
-		
+
 		StringBuffer buffer = new StringBuffer();
-		
+
 		for (int i = 0; i < amount ; i++) {
 			buffer.append(characters[random.nextInt(characters.length-1)]);
 		}
-		
+
 		return buffer.toString();
 	}
-	
+
 }
---
+++ src/test/java/com/monits/commons/RandomUtilsTest.java
@@ -0,0 +1,73 @@
+/**
+ * Random Utils Test
+ *
+ * @copyright	2011 Monits
+ * @license 	Copyright (C) 2011. All rights reserved
+ * @version 	Release: 1.0.0
+ * @link 		http://www.monits.com/
+ * @since 		1.0.0
+ */
+package com.monits.commons;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Random Utils test.
+ *
+ * @author 		Gaston Muñiz <gmuniz@monits.com>
+ * @copyright 	2011 Monits
+ * @license 	Copyright (C) 2011. All rights reserved
+ * @version 	Release: 1.0.0
+ * @link 		http://www.monits.com/
+ * @since 		1.0.0
+ */
+public class RandomUtilsTest {
+
+	private int amount;
+
+	private String actual;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test for the method RandomUtils.generateRandomString(int amount)
+	 */
+	@Test
+	public void testGenerateRandomString() {
+		// Attempt a negative number
+		this.amount = -1;
+		this.actual = RandomUtils.generateRandomString(amount);
+
+		String expected = "";
+		Assert.assertEquals("String build with -1, Failure", expected, actual);
+
+		// Check zero
+		this.amount = 0;
+		this.actual = RandomUtils.generateRandomString(amount);
+
+		Assert.assertEquals("String build with 0, Failure", "", actual);
+
+		// Positive numbers should work
+		amount = 10;
+		this.actual = RandomUtils.generateRandomString(amount);
+
+		Assert.assertNotNull("Null generated, string expected", actual);
+		Assert.assertEquals("Lenght does not match", amount, actual.length());
+	}
+}
