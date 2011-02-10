a3c574caa2258511b9f9079bba66ebfa4aad84b6
Jose Maria Rolon <jmrolon@monits.com>
2011-02-10 18:57:20 +0000

    Doc fixed, this time from the right package

--- src/main/java/com/monits/commons/PaginatedResult.java
+++ src/main/java/com/monits/commons/PaginatedResult.java
@@ -1,7 +1,7 @@
 /**
- * GenericDao interface
+ * PaginatedResult Class
  *
- * @copyright 2010 Monits
+ * @copyright 2011 Monits
  * @license   Copyright (C) 2010. All rights reserved
  * @version   Release: 1.0.0
  * @link      http://www.monits.com/
@@ -12,11 +12,11 @@ package com.monits.commons;
 import java.util.List;
 
 /**
- * GenericDao interface
+ * PaginatedResult Class
  *
  * @author    José Manuel Rolón<jmrolon@monits.com>
- * @copyright 2010 Monits
- * @license   Copyright (C) 2010. All rights reserved
+ * @copyright 2011 Monits
+ * @license   Copyright (C) 2011. All rights reserved
  * @version   Release: 1.0.0
  * @link      http://www.monits.com/
  * @since     1.0.0
@@ -27,20 +27,20 @@ public class PaginatedResult<T> {
 
 	private int totalPage;
 
-	private List<T> pagedList;
+	private List<T> elements;
 
 	/**
 	 * Basic constructor with all fields
 	 *
 	 * @param acutalPage The Selected page
 	 * @param totalPage The total amount of pages
-	 * @param pagedList The items in the selected page
+	 * @param elements The items in the selected page
 	 */
-	public PaginatedResult(int acutalPage, int totalPage, List<T> pagedList) {
+	public PaginatedResult(int acutalPage, int totalPage, List<T> elements) {
 		super();
 		this.acutalPage = acutalPage;
 		this.totalPage = totalPage;
-		this.pagedList = pagedList;
+		this.elements = elements;
 	}
 
 	/**
@@ -58,10 +58,10 @@ public class PaginatedResult<T> {
 	}
 
 	/**
-	 * @return the pagedList
+	 * @return the elements
 	 */
-	public List<T> getPagedList() {
-		return pagedList;
+	public List<T> getElements() {
+		return elements;
 	}
 
 }
