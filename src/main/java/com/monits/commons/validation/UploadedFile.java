f86efdfe3829c2b62d45a4ffdaf211a7acb07deb
Gaston Muñiz <gmuniz@monits.com>
2011-04-27 14:43:32 +0000

    Finally... Annotation! No longer crashes when image is null or empty.
    Maybe we could make a more exhaustive develop for it's validator, such as verifing it contains an extension or verifing extensions itself, ideas to improve then.

---
+++ src/main/java/com/monits/commons/validation/UploadedFile.java
@@ -0,0 +1,45 @@
+/**
+ * ValidFile.java.
+ *
+ * @copyright 2011 Monits
+ * @license Copyright (C) 2011. All rights reserved
+ * @version Release: 1.0.0
+ * @link http://www.monits.com/
+ * @since 1.0.0
+ */
+package com.monits.commons.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * ValidFile.java.
+ *
+ * @author Gaston Muñiz <gmuniz@monits.com>
+ * @copyright 2011 Monits
+ * @license Copyright (C) 2011. All rights reserved
+ * @version Release: 1.0.0
+ * @link http://www.monits.com/
+ * @since 1.0.0
+ */
+@Target({ElementType.FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = UploadedFileValidator.class)
+@Documented
+public @interface UploadedFile {
+
+    String message() default "{constraints.validfile}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
