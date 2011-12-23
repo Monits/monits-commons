5fc9d09e386a298265876b8a331f735c5b47333b
Marco Vanotti <mvanotti@dc.uba.ar>
2011-12-23 16:56:13 -0300

    Fix bug on HashingUtils
    algo.update() was reading the full buffer instead of only the bytes read

--- src/main/java/com/monits/commons/HashingUtils.java
+++ src/main/java/com/monits/commons/HashingUtils.java
@@ -1,7 +1,7 @@
 /*
 
    Copyright 2011 Monits
- 
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
@@ -14,7 +14,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-*/
+ */
 
 
 package com.monits.commons;
@@ -75,7 +75,7 @@ public class HashingUtils {
 			int readBytes;
 			do {
 				readBytes = input.read(buffer);
-				algo.update(buffer);
+				algo.update(buffer, 0, readBytes);
 			} while (readBytes == buffer.length);
 
 			byte messageDigest[] = algo.digest();
