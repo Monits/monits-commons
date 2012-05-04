b47a20bcd0e95eb135c9c36a98bbcf4864aeaf8e
Gaston Nicolas Muñiz <gmuniz@monits.com>
2012-05-04 16:49:26 -0300

    Checklist: Fixed array inicialization

--- src/main/java/com/monits/commons/utils/RandomUtils.java
+++ src/main/java/com/monits/commons/utils/RandomUtils.java
@@ -62,9 +62,9 @@ public class RandomUtils {
 		Random random = new Random();
 
 		char[] characters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-				'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-				'x', 'y', 'z', };
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z', };
 
 		StringBuffer buffer = new StringBuffer();
 
